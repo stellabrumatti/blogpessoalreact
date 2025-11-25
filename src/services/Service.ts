1fef501a82189ecbf26dd0a61ae607886f3b5ced
stella-brumatti <stella.wma@gmail.com>
2025-11-25 17:07:37 -0300

    :sparkles:atualização deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // Instância do Axios
 const api = axios.create({
     //baseURL: 'https://blogpessoal-spring-t83-cwdc.onrender.com'
-    baseURL: 'https://blogpessoal-k3wq.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 // Função para Cadastrar Usuário
